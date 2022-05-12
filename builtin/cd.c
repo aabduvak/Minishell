8f34e941604ab72765730d8c867fba2a18e2dc68
milesdonquijote <milesdonquijote@gmail.com>
2022-05-12 04:22:50 +0300

    pwd cd

--- builtin/cd.c
+++ builtin/cd.c
@@ -3,32 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-<<<<<<< HEAD:includes/macro.h
-/*   By: aabduvak <aabduvak@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/09 19:42:35 by aabduvak          #+#    #+#             */
-/*   Updated: 2022/05/12 03:19:02 by aabduvak         ###   ########.fr       */
-=======
 /*   By: arelmas <arelmas@42istanbul.com.tr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 01:25:16 by arelmas           #+#    #+#             */
-/*   Updated: 2022/05/12 01:25:36 by arelmas          ###   ########.fr       */
->>>>>>> main_arbi:builtin/cd.c
+/*   Updated: 2022/05/12 04:22:05 by arelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-<<<<<<< HEAD:includes/macro.h
-# define SUCCESS 0
-# define FAILED 1
-# define ERROR -1
-
-#endif
-=======
 void	cd(char *path)
 {
 	if (chdir(path) == -1)
 		perror("cd");
 }
->>>>>>> main_arbi:builtin/cd.c
--- builtin/pwd.c
+++ builtin/pwd.c
@@ -3,17 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-<<<<<<< HEAD:libft/sources/ft_strchr.c
-/*   By: aabduvak <aabduvak@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/29 04:46:41 by aabduvak          #+#    #+#             */
-/*   Updated: 2022/05/12 03:05:09 by aabduvak         ###   ########.fr       */
-=======
 /*   By: arelmas <arelmas@42istanbul.com.tr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 01:37:11 by arelmas           #+#    #+#             */
-/*   Updated: 2022/05/12 01:41:27 by arelmas          ###   ########.fr       */
->>>>>>> main_arbi:builtin/pwd.c
+/*   Updated: 2022/05/12 04:22:24 by arelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +14,6 @@
 
 void	pwd()
 {
-<<<<<<< HEAD:libft/sources/ft_strchr.c
-	if (!str)
-		return (NULL);
-	while (*str != (char) c && *str != '\0')
-		str++;
-	if (*str == (char)c)
-		return ((char *)str);
-	return (NULL);
-=======
 	char	*buf;
 
 	buf = getcwd(0, 0);
@@ -40,5 +24,4 @@ void	pwd()
 	}
 	printf("%s\n", buf);
 	free(buf);
->>>>>>> main_arbi:builtin/pwd.c
 }
