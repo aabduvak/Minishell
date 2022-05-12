b288c8ea3dda005552af8727211aedb826244ea6
Abdulaziz Abduvakhobov <aabduvak@k1m09s07.42istanbul.com.tr>
2022-05-12 10:05:59 +0300

    created structs.h

--- includes/structs.h
+++ includes/structs.h
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   macro.h                                            :+:      :+:    :+:   */
+/*   structs.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aabduvak <aabduvak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/12 04:17:34 by aabduvak          #+#    #+#             */
-/*   Updated: 2022/05/12 10:05:22 by aabduvak         ###   ########.fr       */
+/*   Created: 2022/05/12 05:51:22 by aabduvak          #+#    #+#             */
+/*   Updated: 2022/05/12 10:03:09 by aabduvak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MACRO_H
-# define MACRO_H
+#ifndef STRUCTS_H
+# define STRUCTS_H
 
-# define SUCCESS 0
-# define FAILED 1
-# define ERROR -1
+# include <stdlib.h>
 
-# define INVALID_ARGS "invalid argument! Please try again"
-# define INVALID_NUM_ARGS "invalid number of arguments! Please try again"
+typedef struct s_process
+{
+	char				*name;
+	char				**args;
+	char				file_name;
+	int					stdout;
+	int					stdin;
+	int					redirect;
+	pid_t				pid;
+	struct s_process	*next;
+}						t_process;
+
 
 #endif
