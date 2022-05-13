c7cb5aea5723509d97787f8fa9496fd81ff1989b
milesdonquijote <milesdonquijote@gmail.com>
2022-05-13 14:59:41 +0300

    added env variables parser

--- builtin/ft_getenv.c
+++ builtin/ft_getenv.c
@@ -6,11 +6,21 @@
 /*   By: arelmas <arelmas@42istanbul.com.tr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 02:51:48 by arelmas           #+#    #+#             */
-/*   Updated: 2022/05/12 11:23:26 by arelmas          ###   ########.fr       */
+/*   Updated: 2022/05/13 14:34:38 by arelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+static int env_compare(char *s1, char *s2)
+{
+	while (*s1 && *s1 != '=' && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 == '=' && !*s2);
+}
 
 char	*ft_getenv(char *name, t_list *envl)
 {
@@ -27,9 +37,8 @@ char	*ft_getenv(char *name, t_list *envl)
 			continue ;
 		while (env_name[idx] && env_name[idx] != '=')
 			idx++;
-		if (env_name[idx] &&
-				!ft_strncmp(env_name, name, idx++))
-			return (env_name + idx);
+		if (env_name[idx] && env_compare(env_name, name))
+			return (env_name + ++idx);
 		envl = envl->next;
 	}
 	return ("");
--- sources/parsenv.c
+++ sources/parsenv.c
@@ -6,40 +6,95 @@
 /*   By: arelmas <arelmas@42istanbul.com.tr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 11:21:20 by arelmas           #+#    #+#             */
-/*   Updated: 2022/05/12 17:06:36 by arelmas          ###   ########.fr       */
+/*   Updated: 2022/05/13 14:54:18 by arelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-size_t	get_len(t_list *envl, char *input)
+static int	check_first_letter(char c)
+{
+	return (ft_isalpha(c) || c == '_');
+}
+
+static int	check_letter(char c)
+{
+	return (ft_isalpha(c) || ft_isalnum(c) || c == '_');
+}
+
+static void	envcpy(char *dst, char *src, size_t size)
+{
+	if (!src || !dst || !size)
+		return ;
+	while (*src && size--)
+		*dst++ = *src++;
+	*dst = 0;
+}
+
+static size_t	get_len(t_list *envl, char *input)
 {
 	size_t	params_len;
 	size_t	parsed_len;
+	size_t	env_idx;
 	char	*env_name;
 
+	env_idx = 0;
 	parsed_len = 0;
-	while(input[parsed_len])
+	while(input[env_idx])
 	{
 		params_len = 0;
-		if (input[parsed_len + params_len++] == '$')
+		if (input[env_idx + params_len++] == '$')
 		{
-			if (!ft_isalpha(input[parsed_len++ + params_len]))
+			if (!check_first_letter(input[env_idx + params_len++]) && ++env_idx)
 				continue;
-			while (ft_isalpha(input[parsed_len + params_len]) ||
-					ft_isalnum(input[parsed_len + params_len]))
+			while (check_letter(input[env_idx + params_len]))
 				params_len++;
-			env_name = ft_substr(input, parsed_len, params_len);
-			parsed_len += ft_strlen(ft_getenv(env_name, envl)) - 1;
+			env_name = ft_substr(input, env_idx + 1, params_len);
+			env_idx += params_len;
+			parsed_len += ft_strlen(ft_getenv(env_name, envl));
 			free(env_name);
+			parsed_len += 2;
 		}
 		else
+		{
+			env_idx++;
 			parsed_len++;
+		}
 	}
 	return (parsed_len);
 }
 
 char	*parsenv(t_list *envl, char *input)
 {
-	return (0);
+	size_t	idx;
+	size_t	p_len;
+	char	*parsed;
+	char	*env_name;
+
+	if (!input)
+		return (0);
+	idx = 0;
+	p_len = get_len(envl, input);
+	parsed = (char *)malloc(sizeof(char) * (p_len + 1));
+	if (!parsed)
+		return (0);
+	while (*input)
+	{
+		p_len = 0;
+		if (input[p_len++] == '$' && check_first_letter(input[p_len++]))
+		{
+			while (check_letter(*(input + p_len)))
+				p_len++;
+			env_name = ft_substr(input, 1, p_len - 1);
+			parsed[idx++] = '\"';
+			envcpy(parsed + idx, ft_getenv(env_name, envl), ft_strlen(ft_getenv(env_name, envl)));
+			idx += ft_strlen(ft_getenv(env_name, envl));
+			parsed[idx++] = '\"';
+			input += p_len;
+		}
+		else
+			parsed[idx++] = *input++;
+	}
+	parsed[idx] = 0;
+	return (parsed);
 }
